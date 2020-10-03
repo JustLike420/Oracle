6ff62ab3cd132ed282eb11504621e38745d18069
JustLike420 <justlikevova@gmail.com>
2020-10-03 20:04:21 +0300

    добавил функцию поиска названия года по кит. календарю

--- Oracle.cpp
+++ Oracle.cpp
@@ -2,165 +2,20 @@
 #include <locale.h>
 
 int zod;
-int days(int year, int month, int d)
-{
-	int day_d = 0;
-	int days_m_b = 0;
-	while (month > day_d)
-	{
-		switch (day_d)
-		{
-		case 0:
-			days_m_b += 31;
-			if (d < 20)
-			{
-				zod = 1; //Capricorn
-			}
-			else
-			{
-				zod = 2; //Aquarius
-			}
-			break;
-		case 1:
-			days_m_b += 28;
-			if (year % 4 == 0)
-			{
-				days_m_b++;
-			}
-			if (d > 19)
-			{
-				zod = 3; //Pisces
-			}
-			break;
-		case 2:
-			days_m_b += 31;
-			if (d > 20)
-			{
-				zod = 4; //Aries
-			}
-			break;
-		case 3:
-			days_m_b += 30;
-			if (d > 20)
-			{
-				zod = 5; //Taurus
-			}
-			else
-			{
-				zod = 4; //Aries
-			}
-			break;
-		case 4:
-			days_m_b += 31;
-			if (d > 22)
-			{
-				zod = 6; //Gemini
-			}
-			break;
-		case 5:
-			days_m_b += 30;
-			if (d > 21)
-			{
-				zod = 7; //Cancer
-			}
-			break;
-		case 6:
-			days_m_b += 31;
-			if (d > 23)
-			{
-				zod = 8; // Leo
-			}
-			break;
-		case 7:
-			days_m_b += 31;
-			if (d > 24)
-			{
-				zod = 9; //Vrigo
-			}
-			break;
-		case 8:
-			days_m_b += 30;
-			if (d > 24)
-			{
-				zod = 10; //Libra
-			}
-			break;
-		case 9:
-			days_m_b += 31;
-			if (d > 24)
-			{
-				zod = 11; //Scorpio
-			}
-			break;
-		case 10:
-			days_m_b += 30;
-			if (d < 23)
-			{
-				zod = 12; //Sagittarius
-			}
-			break;
-		case 11:
-			days_m_b += 31;
-			if (d > 23)
-			{
-				zod = 1; // Capricorn
-			}
-			break;
-		}
-		day_d++;
-	}
-	return days_m_b;
-}
-
+void z_god(int mon, int day); // знак зодиака по китайскому календарю
+int days(int year, int month, int d);
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	int mon;
-	int today_d, today_m, today_y, birth_d, birth_m, birth_y;
-	printf("Привет мир!");
-	printf("Please enter\nwhat date today: dd.mm.yyyy\ndate of your birth: dd.mm.yyyy\n");
+	setlocale(LC_ALL, "Russian");  //русский язык
+
+	int today_d, today_m, today_y, birth_d, birth_m, birth_y, mon;
+
+	printf("Пожалуйста введите следующие данные:\nСегодняшняя дата: дд.мм.гг\nДату совего дня рождения: дд.мм.гг\n");
 	scanf("%d%d%d", &today_d, &today_m, &today_y);
 	scanf("%d%d%d", &birth_d, &birth_m, &birth_y);
-	int ch_year = (birth_y % 12) - 1;
-	switch (ch_year)
-	{
-	case 0:
-		mon = 1; //cock
-		break;
-	case 1:
-		mon = 2; //dog
-		break;
-	case 2:
-		mon = 3; //pig
-		break;
-	case 3:
-		mon = 4; //rat
-		break;
-	case 4:
-		mon = 5; //bull
-		break;
-	case 5:
-		mon = 6; //tiger
-		break;
-	case 6:
-		mon = 7; //rabbit
-		break;
-	case 7:
-		mon = 8; //dragon
-		break;
-	case 8:
-		mon = 9; //snake
-		break;
-	case 9:
-		mon = 10; //horse
-		break;
-	case 10:
-		mon = 11; //goat
-		break;
-	case 11:
-		mon = 12; //monkey
-		break;
-	}
+	
+	printf("Ваш год по китайсому календарю:");              // Вывод названия года по китайскому календарю
+	z_god(birth_y);                                         
 
 	int year = birth_y;
 	int days_v = 0;
@@ -274,4 +129,158 @@ int main()
 		printf("Not leap year");
 	}
 	printf("Programm was created by Vladimir Chigin"); // https://vk.com/just_like248
+}
+
+int days(int year, int month, int d){
+	int day_d = 0;
+	int days_m_b = 0;
+	while (month > day_d)
+	{
+		switch (day_d)
+		{
+		case 0:
+			days_m_b += 31;
+			if (d < 20)
+			{
+				zod = 1; //Capricorn
+			}
+			else
+			{
+				zod = 2; //Aquarius
+			}
+			break;
+		case 1:
+			days_m_b += 28;
+			if (year % 4 == 0)
+			{
+				days_m_b++;
+			}
+			if (d > 19)
+			{
+				zod = 3; //Pisces
+			}
+			break;
+		case 2:
+			days_m_b += 31;
+			if (d > 20)
+			{
+				zod = 4; //Aries
+			}
+			break;
+		case 3:
+			days_m_b += 30;
+			if (d > 20)
+			{
+				zod = 5; //Taurus
+			}
+			else
+			{
+				zod = 4; //Aries
+			}
+			break;
+		case 4:
+			days_m_b += 31;
+			if (d > 22)
+			{
+				zod = 6; //Gemini
+			}
+			break;
+		case 5:
+			days_m_b += 30;
+			if (d > 21)
+			{
+				zod = 7; //Cancer
+			}
+			break;
+		case 6:
+			days_m_b += 31;
+			if (d > 23)
+			{
+				zod = 8; // Leo
+			}
+			break;
+		case 7:
+			days_m_b += 31;
+			if (d > 24)
+			{
+				zod = 9; //Vrigo
+			}
+			break;
+		case 8:
+			days_m_b += 30;
+			if (d > 24)
+			{
+				zod = 10; //Libra
+			}
+			break;
+		case 9:
+			days_m_b += 31;
+			if (d > 24)
+			{
+				zod = 11; //Scorpio
+			}
+			break;
+		case 10:
+			days_m_b += 30;
+			if (d < 23)
+			{
+				zod = 12; //Sagittarius
+			}
+			break;
+		case 11:
+			days_m_b += 31;
+			if (d > 23)
+			{
+				zod = 1; // Capricorn
+			}
+			break;
+		}
+		day_d++;
+	}
+	return days_m_b;
+}
+
+ // знак зодиака по китайскому календарю
+void z_god(int y){
+	int god;
+	god = y % 12;
+	switch (god)
+	{
+	case 0:
+		printf("Год обезьяны");
+		break;
+	case 1:
+		printf("Год петуха");
+		break;
+	case 2:
+		printf("Год собаки");
+		break;
+	case 3:
+		printf("Год свиньи");
+		break;
+	case 4:
+		printf("Год крысы");
+		break;
+	case 5:
+		printf("Год быка");
+		break;
+	case 6:
+		printf("Год тигра");
+		break;
+	case 7:
+		printf("Год кролика");
+		break;
+	case 8:
+		printf("Год дракона");
+		break;
+	case 9:
+		printf("Год змеи");
+		break;
+	case 10:
+		printf("Год лошади");
+		break;
+	case 11:
+		printf("Год козы");
+		break;
+	}
 }
